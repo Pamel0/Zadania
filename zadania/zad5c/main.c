260f79fa0f1dbc37c662bd081ea65af7f0e56c88
Pamel0 <93038245+Pamel0@users.noreply.github.com>
2021-11-12 17:58:36 +0100

    Update main.c

--- zadania/zad5c/main.c
+++ zadania/zad5c/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-void wolaj_a(void); /* deklaracja funkcji nie pobierajacej zadnych argumentow i nie
-		       zwracajacej zadnej wartosci */
-void wolaj_b(void); /* j.w. */
+void wolaj_a(void); 
+void wolaj_b(void); 
 
 int main(void)
 {
-  wolaj_a(); /* wywolanie funkcji */
+  wolaj_a(); 
   wolaj_a();
   wolaj_b();
 
@@ -14,12 +13,12 @@ int main(void)
 }
 
 
-void wolaj_a(void) /* definicja funkcji wolaj_a */
+void wolaj_a(void) 
 {
   printf("Panie Janie!\n");
 }
 
-void wolaj_b(void) /*definicja funkcji wolaj_b */
+void wolaj_b(void) 
 {
   printf("Rano wstan!\n");
 }
