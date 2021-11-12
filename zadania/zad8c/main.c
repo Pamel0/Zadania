84e0ab176e8276ce913fc92f63c5124c8a04e4cb
Pamel0 <93038245+Pamel0@users.noreply.github.com>
2021-11-12 17:59:10 +0100

    Update main.c

--- zadania/zad8c/main.c
+++ zadania/zad8c/main.c
@@ -5,8 +5,8 @@ int main(void)
   int kod;
 
   printf("Podaj kod ASCII: \n");
-  scanf("%d", &kod); /* pobiera liczbe calkowita z wejscia */
-  printf("%c\n", kod); /* wypisuje znak odpowiadajacy pobranej liczbie calkowitej */
+  scanf("%d", &kod); 
+  printf("%c\n", kod); 
 
   return 0;
 }
